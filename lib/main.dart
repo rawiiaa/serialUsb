f72e7ebfe48d0718b65e84d2daf7c306f402fa9d
unknown <chafairihab@gmail.com>
2022-09-07 16:57:12 +0200

    serialusb

--- lib/main.dart
+++ lib/main.dart
@@ -20,45 +20,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
-
   Timer? timer;
-  List<_ChartData>? chartData;
-  List<_ChartTest>? chartTest;
-
-  late int count;
-  ChartSeriesController? _chartSeriesController;
-  ChartSeries? _chartSeries;
-
-  @override
-
-  @override
-
   UsbPort? _port;
   String _status = "Idle";
   List<Widget> _ports = [];
   List _serialData = [];
-  List _serialusb= [];
-  List<double> traceX =[];
-  List<double> essai =[200.30,150.0,800.2,100.5];
-  int i = 0;
+  List<double> traceX = [];
   int ecg = 0;
-  List<double> traceSine = [];
-  List<double> traceCosine = [];
-  double radians = 0.0;
-  Timer? _timer;
+  double test = 0.0;
 
   /// method to generate a Test  Wave Pattern Sets
   /// this gives us a value between +1  & -1 for sine & cosine
-
-
 
   StreamSubscription<String>? _subscription;
   Transaction<String>? _transaction;
   UsbDevice? _device;
 
-  TextEditingController _textController = TextEditingController();
 
   Future<bool> _connectTo(device) async {
     _serialData.clear();
@@ -86,7 +64,7 @@ class _MyAppState extends State<MyApp> {
       return true;
     }
     _port = await device.create();
-    debugPrint('portsssss'+_port.toString());
+    debugPrint('portsssss' + _port.toString());
 
     if (await (_port!.open()) != true) {
       setState(() {
@@ -99,37 +77,24 @@ class _MyAppState extends State<MyApp> {
     await _port!.setDTR(true);
     await _port!.setRTS(true);
 
-    _port!.setPortParameters(9600, UsbPort.DATABITS_8,
-        UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+    _port!.setPortParameters(
+        9600, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
 
     // print first result and close port.
     _port!.inputStream!.listen((Uint8List event) {
       setState(() {
         String s = new String.fromCharCodes(event);
         try {
-        ecg = int.parse(s);
-
-           double test = double.parse(ecg.toString());
-           double tt = test /10000000;
-           _serialData.add(Text(test.toString()));
-           traceX.add(test);
-           debugPrint('ECGG'+traceX.toString());
-           debugPrint('Value'+test.toString());
-         }catch (error) {
+          test = double.parse(s);
+          debugPrint('Value' + test.toString());
+        } catch (error) {
           print(error);
         }
 
 
-        /*_serialusb.add(ecg);
-        debugPrint('ECG'+ecg.toString());
-          chartData?.add(_ChartData(i++,ecg));*/
-
-
-
-
       });
 
-      _port!.close();
+      //_port!.close();
     });
 
     await _port!.write(Uint8List.fromList([0x10, 0x00]));
@@ -139,14 +104,13 @@ class _MyAppState extends State<MyApp> {
     return true;
   }
 
-
   /// method to generate a Test  Wave Pattern Sets
   /// this gives us a value between +1  & -1 for sine & cosine
 
   void _getPorts() async {
     _ports = [];
     List<UsbDevice> devices = await UsbSerial.listDevices();
-    debugPrint('devicessss'+devices.toString());
+    debugPrint('devicessss' + devices.toString());
     if (!devices.contains(_device)) {
       _connectTo(null);
     }
@@ -156,8 +120,8 @@ class _MyAppState extends State<MyApp> {
       _ports.add(ListTile(
           leading: Icon(Icons.usb),
           title: Text(device.productName!),
-
           trailing: ElevatedButton(
+          
             child: Text(_device == device ? "Disconnect" : "Connect"),
             onPressed: () {
               _connectTo(_device == device ? null : device).then((res) {
@@ -169,25 +133,33 @@ class _MyAppState extends State<MyApp> {
 
     setState(() {
       print(_ports);
-
     });
   }
+
   @override
   void initState() {
     super.initState();
+
+    var duration = const Duration(milliseconds: 100);
+
+    Timer.periodic(duration, (timer) {
+      print("timer"+test.toString());
+      setState(() {
+        if (test != 0.0) {
+          traceX.add(test);
+        }
+      });
+    });
+
     UsbSerial.usbEventStream!.listen((UsbEvent event) {
       _getPorts();
     });
-
-
+    _getPorts();
   }
 
   @override
   void dispose() {
     timer?.cancel();
-    chartData!.clear();
-    _chartSeriesController = null;
-    _chartSeries = null;
 
     super.dispose();
     _connectTo(null);
@@ -201,117 +173,34 @@ class _MyAppState extends State<MyApp> {
       yAxisColor: Colors.orange,
       //margin: EdgeInsets.all(5.0),
       strokeWidth: 2.0,
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       traceColor: Colors.green,
-      yAxisMax: 6500000.0,
-      yAxisMin: 6100000.0,
+      yAxisMax: 6450000.0,
+      yAxisMin: 6430000.0,
       dataSet: traceX,
     );
-    debugPrint('Chart'+traceX.toString());
 
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text('USB Serial Plugin example app'),
-          ),
-          body:
-                  Column(children: <Widget>[
+      appBar: AppBar(
+        title: const Text('USB Serial Plugin example app'),
+      ),
+      body: Column(children: <Widget>[
+        Text(
+            _ports.length > 0
+                ? "Available Serial Ports"
+                : "No serial devices available",
+            style: Theme.of(context).textTheme.headline6),
+        ..._ports,
+         traceX != 0.0
+        ? Container(
+                height: 500,
+                width: 1000,
+                child: scopeOne,
+              ):Container()
 
-                      Material(
-                          color: Colors.transparent,
-                          child: Ink(
-                              decoration: BoxDecoration(
-                                color:Color.fromRGBO(64, 184, 233, 1),
-                                borderRadius: BorderRadius.circular(70),
-                              ),
-                              child: InkWell(
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(builder: (context) =>  Chart()),
-                                    );
-                                  },
-                                  child: Container(
-                                      width: MediaQuery
-                                          .of(context)
-                                          .size
-                                          .width,
-                                      height: 40,
-                                      padding: const EdgeInsets.only(
-                                          top: 8,
-                                          bottom: 9,
-                                          left: 5,
-                                          right: 5),
-
-                                      child: Text(
-                                        'Chart',
-                                        textAlign: TextAlign.center,
-
-                                      ))))),
-                      Text(_ports.length > 0 ? "Available Serial Ports" : "No serial devices available", style: Theme.of(context).textTheme.headline6),
-                      ..._ports,
-                      Text('info: ${_port.toString()}\n'),
-                         _serialData.isNotEmpty
-                         ?Container(
-                           height: 500,
-                           width: 400,
-                             child: scopeOne,
-                         )
-                             :Container()
-
-                    ]),
-        ));
-  }
-  /*SfCartesianChart _buildLiveLineChart() {
-
-   return SfCartesianChart(
-
-     series:
-     _serialData.isNotEmpty
-
-     ?<LineSeries<_ChartData, int>>[
-
-        LineSeries<_ChartData, int>(
-          onRendererCreated: (ChartSeriesController controller) {
-            // Assigning the controller to the _chartSeriesController.
-            _chartSeriesController = controller;
-          },
-          // Binding the chartData to the dataSource of the line series.
-          dataSource: chartData!,
-          xValueMapper: (_ChartData sales, _) => sales.country,
-          yValueMapper: (_ChartData sales, _) => sales.sales,
-        )
+      ]),
+    ));
+  }}
 
 
-      ]:<LineSeries<_ChartTest, int>>[
-
-       LineSeries<_ChartTest, int>(
-         onRendererCreated: (ChartSeriesController controller) {
-           // Assigning the controller to the _chartSeriesController.
-           _chartSeriesController = controller;
-         },
-         // Binding the chartData to the dataSource of the line series.
-         dataSource: chartTest!,
-         xValueMapper: (_ChartTest sales, _) => sales.test1,
-         yValueMapper: (_ChartTest sales, _) => sales.test2,
-       )
-
-
-     ]
-     ,
-    )
-    ;
-  }*/
-
-
-}
-class _ChartData {
-  _ChartData(this.country, this.sales);
-  final int country;
-  final num sales;
-}
-class _ChartTest {
-  _ChartTest(this.test1, this.test2);
-  final int test1;
-  final num test2;
-}
